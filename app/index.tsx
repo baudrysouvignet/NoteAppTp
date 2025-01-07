5b3e407d09b5bb0b2f731cacf8250484a8eae741
baudrysouvignet <baudrysouvignet07@gmail.com>
2025-01-07 08:43:05 +0100

    C1

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { Card } from "./components/Card";
+
+export default function Index() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
